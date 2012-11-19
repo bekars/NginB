1c3a6f7f560a448dc31f2a90125d38e572dc01c2
baiyu <baiyu@BLW.(none)>
2012-11-19 10:16:38 +0800

    update.

--- build/build_ngx.sh
+++ build/build_ngx.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-./configure --with-debug --add-module=./mod_minify --with-http_stub_status_module
+NGX_PATH=../nginx-1.2.4
+NGX_MOD_PATH=../modules
+
+cd ${NGX_PATH}
+./configure --with-debug --add-module=${NGX_MOD_PATH}/mod_minify --with-http_stub_status_module
+make
+
