c985a9a599bea58a8a1652d8c20f222fd1e806d6
bekars <bekars@gmail.com>
2012-11-21 16:17:03 +0800

    add Makefile.

--- build/build_ngx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NGX_PATH=../nginx-1.2.4
-NGX_MOD_PATH=../modules
-
-cd ${NGX_PATH}
-./configure --with-debug --add-module=${NGX_MOD_PATH}/mod_minify --with-http_stub_status_module
-make
-
