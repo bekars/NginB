519c79b5bd363d013fcef9437f5f436d609b6108
bekars <bekars@gmail.com>
2012-11-28 18:16:11 +0800

    update

--- scripts/capcache.pl
+++ scripts/capcache.pl
@@ -7,6 +7,11 @@ my $startime = new Benchmark;
 my $logfile = "www.domain.com_80.log";
 
 #
+# Rate - 各种头域出现的比率
+# MISS Res - 未命中缓存资源
+# No-Cache Res - 未缓存资源 ==> log
+#
+#
 # Log Format
 # 127.0.0.1 - - [27/Nov/2012:12:10:59 +0800] "GET http://www.google-analytics.com/__utm.gif?utmwv=5.3.8&utms=1&utmn=2097981030&utmhn=www.anquanbao.com HTTP/1.1" 200 35 "http://www.anquanbao.com/" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:17.0) Gecko/17.0 Firefox/17.0" "-" "-" MISS "Wed, 19 Apr 2000 11:43:00 GMT" "private, no-cache, no-cache=Set-Cookie, proxy-revalidate" "AAAAAAAAAAA" "Wed, 21 Jan 2004 19:51:30 GMT" 0.085 0.085 "-"
 #
@@ -14,29 +19,36 @@ my $logfile = "www.domain.com_80.log";
 # domain time url http-status length cache-status expired cache-control etag last-modified
 #
 #                       1           2         3       4                                                           5         6           7           8           9
-my $match_str = qr/.*?\[(.*?)\].*?\"(.*?)\"\s+(.*?)\s+(.*?)\s+\"[^\"]+\"\s+\"[^\"]+\"\s+\"[^\"]+\"\s+\"[^\"]+\"\s+(.*?)\s+\"(.*?)\"\s+\"(.*?)\"\s+\"(.*?)\"\s+\"(.*?)\"\s+.*/;
+my $log_reg = qr/.*?\[(.*?)\].*?\"(.*?)\"\s+(.*?)\s+(.*?)\s+\"[^\"]+\"\s+\"[^\"]+\"\s+\"[^\"]+\"\s+\"[^\"]+\"\s+(.*?)\s+\"(.*?)\"\s+\"(.*?)\"\s+\"(.*?)\"\s+\"(.*?)\"\s+.*/;
 
 sub do_exit
 {
-    my $logstr = shift;
-    die "$logstr\n";
+    my $str = shift;
+    die "$str\n";
 }
 
 sub analysis
 {
-    my $data = shift;
-    print(join "|", @$data);
+    my $log_data_a = shift;
+    print(join "|", @$log_data_a);
+    print("\n\n");
+ 
+    my %stat_h = (
+        time => @$log_data_a[0],
+        url => @$log_data_a[1],
+    );
+    print("$stat_h{time}\n\n");
 }
 
 sub parse_log
 {
-    my ($filename, $func) = @_;
+    my ($filename, $func, $reg) = @_;
 
     open(FILEHANDLE, $filename) or do_exit("Can not open file $filename !");
 
     while (<FILEHANDLE>) {
-        print("$_\n");
-        my @line = ($_ =~ m/$match_str/);
+        #print("$_");
+        my @line = ($_ =~ m/$reg/);
         if ($#line > 0) {
             &{$func}(\@line);
         } else {
@@ -47,7 +59,7 @@ sub parse_log
     close(FILEHANDLE);
 }
 
-parse_log($logfile, \&analysis);
+parse_log($logfile, \&analysis, $log_reg);
 
 printf "\n\n### %s ###\n\n", timestr(timediff(new Benchmark, $startime));
 
