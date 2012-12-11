1b9afd850ff2fdd2638afdacf0c726b7dfaf129d
bekars <bekars@gmail.com>
2012-12-11 16:36:26 +0800

    update.

--- scripts/capcache.pl
+++ scripts/capcache.pl
@@ -295,6 +295,139 @@ sub get_maxage_interval
     return 0;
 }
 
+my @ttl_a = (
+    {
+        name => "1h",
+        min => 0,
+        max => 3600,
+    },
+    {
+        name => "2h",
+        min => 3600,
+        max => 7200,
+    },
+    {
+        name => "3h",
+        min => 7200,
+        max => 10800,
+    },
+    {
+        name => "4h",
+        min => 10800,
+        max => 14400,
+    },
+    {
+        name => "5h",
+        min => 14400,
+        max => 18000,
+    },
+    {
+        name => "6h",
+        min => 18000,
+        max => 21600,
+    },
+    {
+        name => "8h",
+        min => 21600,
+        max => 28800,
+    },
+    {
+        name => "12h",
+        min => 28800,
+        max => 43200,
+    },
+    {
+        name => "16h",
+        min => 43200,
+        max => 57600,
+    },
+    {
+        name => "20h",
+        min => 57600,
+        max => 72000,
+    },
+    {
+        name => "1d",
+        min => 72000,
+        max => 86400,
+    },
+    {
+        name => "2d",
+        min => 86400,
+        max => 172800,
+    },
+    {
+        name => "3d",
+        min => 172800,
+        max => 259200,
+    },
+    {
+        name => "4d",
+        min => 259200,
+        max => 345600,
+    },
+    {
+        name => "5d",
+        min => 345600,
+        max => 432000,
+    },
+    {
+        name => "6d",
+        min => 432000,
+        max => 518400,
+    },
+    {
+        name => "8d",
+        min => 518400,
+        max => 691200,
+    },
+    {
+        name => "16d",
+        min => 691200,
+        max => 1382400,
+    },
+    {
+        name => "24d",
+        min => 1382400,
+        max => 2073600,
+    },
+    {
+        name => "1m",
+        min => 2073600,
+        max => 2592000,
+    },
+    {
+        name => "2m",
+        min => 2592000,
+        max => 5184000,
+    },
+    {
+        name => "3m",
+        min => 5184000,
+        max => 7776000,
+    },
+    {
+        name => "6m",
+        min => 7776000,
+        max => 15552000,
+    },
+    {
+        name => "9m",
+        min => 15552000,
+        max => 23328000,
+    },
+    {
+        name => "1y",
+        min => 23328000,
+        max => 31104000,
+    },
+    {
+        name => ">1y",
+        min => 31104000,
+        max => 9999999999,
+    },
+);
+
 sub expired_analysis_mod
 {
     my $node_h = shift;
@@ -321,10 +454,31 @@ sub expired_analysis_mod
     }
 
     if ($interval > 0) {
-        $interval = int($interval / 3600);
-        $expired_h{$interval . "h"} += 1;
-        $expired_h{$interval . "h_FLOW"} += $node_h->{http_len};
+        for my $index (0..$#ttl_a) {
+            if (($interval > $ttl_a[$index]{min}) && 
+                ($interval <= $ttl_a[$index]{max}))
+            {
+                $expired_h{$ttl_a[$index]{name}} += 1;
+                $expired_h{$ttl_a[$index]{name} . "_FLOW"} += $node_h->{http_len};
+                last;
+            }
+        }
     }
+}
+
+sub dump_log_expired
+{
+    open(EXPIRED_FILE, ">ttl.log");
+
+    for my $index (0..$#ttl_a) {
+        if (exists($expired_h{$ttl_a[$index]{name}})) {
+            printf(EXPIRED_FILE "$ttl_a[$index]{name}\t" . 
+                "$expired_h{$ttl_a[$index]{name} . '_FLOW'}\t" .
+                "$expired_h{$ttl_a[$index]{name}}\n");
+        }
+    }
+
+    close(EXPIRED_FILE);
 }
 
 sub dump_mod
@@ -584,6 +738,8 @@ show_hash(\%nocache_http_header_h, "NOCACHE_HEADER");
 show_hash(\%nocache_http_sufix_h, "NOCACHE_SUFIX");
 show_hash(\%html_http_header_h, "HTML_HEADER");
 show_hash(\%expired_h, "EXPIRED_TTL");
+
+dump_log_expired();
 
 if (exists($options{T})) {
     printf "\n\n### %s ###\n\n", timestr(timediff(new Benchmark, $startime));
