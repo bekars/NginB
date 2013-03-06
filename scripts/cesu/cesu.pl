e6e21e5a9ee92725b23f9d5cd43d51b59d86ce00
bekars <bekars@gmail.com>
2013-03-06 15:40:11 +0800

    add cesu.pl.

---
+++ scripts/cesu/cesu.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Speedy::AQB;
+use Speedy::Utils;
+use Speedy::Http;
+use Data::Dumper;
+
+require "sites.pl";
+
+# delete config cache=off site from cesu result
+sub removeCacheOff()
+{
+    my $inf = "/home/baiyu/Dropbox/Cesu/results/speed_sort.2013-03-02~2013-03-03.txt";
+    my $outf = "cesu.result";
+
+    open(INFD, $inf);
+    open(OUTFD, ">" . $outf);
+
+    while (<INFD>)
+    {
+        my @arr = split(/ /, $_);
+        removeRN(\$arr[1]);
+
+        my $site = getSiteInfo($arr[1]);
+        if ((!exists($site->{config}->{cache})) ||
+            (exists($site->{config}->{cache}) && ($site->{config}->{cache} eq "on"))) 
+        {
+            printf($arr[0]."\t".$arr[1]."\n");
+        }
+    }
+
+    close(INFD);
+    close(OUTFD);
+}
+
+# mainpage is dyn-page ?
+sub 
+
+showHash($cesusites::sites);
+
+1;
+
