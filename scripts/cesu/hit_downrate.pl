babad6b29f11ab550a03666e0d1f75e66965d85b
bekars <bekars@gmail.com>
2013-04-23 18:40:53 +0800

    update.

--- scripts/cesu/hit_downrate.pl
+++ scripts/cesu/hit_downrate.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/perl -w
 
+# 统计HIT之后下载比源站慢的节点
+
 use strict;
 use Speedy::Utils;
 use Speedy::Http;
 use Data::Dumper;
+use IO::Handle;
 
 use DBI;
 
 my $dr_sites = {};
-my $date_g = "2013-03-25";
+my $date_g = "2013-03-24";
+my $fp;
 
 my $dbh;
 my ($dbhost, $dbuser, $dbpass, $dbname, $dbport);
@@ -46,7 +50,7 @@ sub runSQL($)
         return;
     }
 
-    printf("RUNSQL: $sql\n");
+    #printf("RUNSQL: $sql\n");
     my $sth = $dbh->prepare($sql);
     $sth->execute() or die("SQL err: [$sql]" . "(" . length($sql) .")" . $sth->errstr);
     while (@row = $sth->fetchrow_array) {
@@ -66,19 +70,21 @@ use constant {
     ROLE_IP     => 3,
     URL         => 4,
     HEADER      => 5,
+    CLI_IP      => 6,
+    CLI_TYPE    => 7,
 };
 sub dr_one_site_elements($)
 {
     my $site_ref = shift;
     my $cluster = "";
     
-    my $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
+    my $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header, client_ip, client_type from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
         $site_ref->{aqb}{role_id}, $site_ref->{aqb}{monitor_time});
     my $recs_aqb = runSQL($sql);
     
-    $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
-        $site_ref->{ip}{role_id}, $site_ref->{ip}{monitor_time});
-    my $recs_ip = runSQL($sql);
+    $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header, client_ip, client_type from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
+        $site_ref->{org}{role_id}, $site_ref->{org}{monitor_time});
+    my $recs_org = runSQL($sql);
 
     for (my $i = 0; $i <= $#$recs_aqb; $i++) {
         if ($recs_aqb->[$i]->[HEADER] =~ m/.*\^X-Powered-By-Anquanbao: HIT from (.*?)\^.*$/) {
@@ -88,17 +94,21 @@ sub dr_one_site_elements($)
                 $recs_aqb->[$i]->[DOWN_TIME] = -1;
             }
 
-            LOOP: for (my $j = 0; $j <= $#$recs_ip; $j++) {
-                if ($recs_ip->[$j]->[URL] eq $recs_aqb->[$i]->[URL]) {
-                    if ($recs_ip->[$j]->[DOWN_TIME] == 0) {
-                        $recs_ip->[$j]->[DOWN_TIME] = -1;
+            LOOP: for (my $j = 0; $j <= $#$recs_org; $j++) {
+                if ($recs_org->[$j]->[URL] eq $recs_aqb->[$i]->[URL]) {
+                    if ($recs_org->[$j]->[DOWN_TIME] == 0) {
+                        $recs_org->[$j]->[DOWN_TIME] = -1;
                     }
 
-                    if (($recs_aqb->[$i]->[DOWN_TIME]/$recs_ip->[$j]->[DOWN_TIME]) > 1.5) {
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb_dtime} = $recs_aqb->[$i]->[DOWN_TIME];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org_dtime} = $recs_ip->[$j]->[DOWN_TIME];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb_ip} = $recs_aqb->[$i]->[ROLE_IP];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org_ip} = $recs_ip->[$j]->[ROLE_IP];
+                    if (($recs_aqb->[$i]->[DOWN_TIME]>1) && ($recs_aqb->[$i]->[DOWN_TIME]/$recs_org->[$j]->[DOWN_TIME]) > 1.5) {
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{downtime} = $recs_aqb->[$i]->[DOWN_TIME];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{downtime} = $recs_org->[$j]->[DOWN_TIME];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{ip} = $recs_aqb->[$i]->[ROLE_IP];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{ip} = $recs_org->[$j]->[ROLE_IP];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{cli_ip} = $recs_aqb->[$i]->[CLI_IP];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{cli_ip} = $recs_org->[$j]->[CLI_IP];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{cli_type} = $recs_aqb->[$i]->[CLI_TYPE];
+                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{cli_type} = $recs_org->[$j]->[CLI_TYPE];
                     }
 
                     last LOOP;
@@ -134,30 +144,71 @@ sub dr_one_site($$)
     
     $recs = runSQL($sql);
     for (my $i = 0; $i <= $#$recs; $i++) {
-        $dr_sites->{$site}{ip}{role_id} = $recs->[$i]->[ROLE_ID];
-        $dr_sites->{$site}{ip}{total_time} = $recs->[$i]->[TOTAL_TIME];
-        $dr_sites->{$site}{ip}{city_code} = $recs->[$i]->[CITY_CODE];
-        $dr_sites->{$site}{ip}{monitor_time} = $recs->[$i]->[MONITOR_TIME];
+        $dr_sites->{$site}{org}{role_id} = $recs->[$i]->[ROLE_ID];
+        $dr_sites->{$site}{org}{total_time} = $recs->[$i]->[TOTAL_TIME];
+        $dr_sites->{$site}{org}{city_code} = $recs->[$i]->[CITY_CODE];
+        $dr_sites->{$site}{org}{monitor_time} = $recs->[$i]->[MONITOR_TIME];
     }
 
-    if (($dr_sites->{$site}{aqb}{total_time}/$dr_sites->{$site}{ip}{total_time}) > 1.5) {
-        dr_one_site_elements($dr_sites->{$site});
+    if ($#$recs != -1) {
+        if (($dr_sites->{$site}{aqb}{total_time}/$dr_sites->{$site}{org}{total_time}) > 1.5) {
+            dr_one_site_elements($dr_sites->{$site});
+        }
     }
 
-    print Dumper($dr_sites);
+    print Dumper($dr_sites->{$site});
     
 }
 
-    
+sub log_one_site($)
+{
+    my $site_k = shift;
+    if (exists($dr_sites->{$site_k}) && exists($dr_sites->{$site_k}->{clusters})) {
+        my $len = keys %{$dr_sites->{$site_k}->{clusters}};
+        if ($len > 0) { 
+            printf($fp "[$site_k]\n");
+            foreach my $srv_k (sort keys %{$dr_sites->{$site_k}->{clusters}}) {
+                printf($fp "$srv_k\n");
+                foreach my $e (sort keys %{$dr_sites->{$site_k}->{clusters}->{$srv_k}}) {
+                    printf($fp "\tURL: $e\n");
+                    printf($fp "\tORG: $dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{ip}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{downtime}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{cli_ip}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{cli_type}\n");
+                    printf($fp "\tAQB: $dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{ip}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{downtime}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{cli_ip}, " .
+                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{cli_type}\n");
+                }
+            }
+        }
+    }
+}
+
+
+open($fp, ">hit_download.result");
+$fp->autoflush(1);
+
 my $driver  = "DBI:mysql";
 load_db_config();
 # database connect
 $dbh = DBI->connect("$driver:database=$dbname;host=$dbhost;user=$dbuser;password=$dbpass;port=$dbport") or die("ConnDB err: " . DBI->errstr);
 
-
-dr_one_site("www.jd.cn", 12);
+open(my $fcesu, "<cesu.result");
+while (<$fcesu>) {
+    if (m/(.*?)\t(.*?)$/) {
+        if ($1 < 0) {
+            printf("### analysis $2 ...\n");
+            dr_one_site($2, 12);
+            log_one_site($2);
+        }
+    }
+}
+close($fcesu);
 
 $dbh->disconnect();
+
+close($fp);
 
 1;
 
