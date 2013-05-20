2fe025e6c3a54cede3c3f575ccb54f0aceb99189
bekars <bekars@gmail.com>
2013-05-20 20:03:21 +0800

    update.

--- scripts/cesu/hit_downspeed.pl
+++ scripts/cesu/hit_downspeed.pl
@@ -13,206 +13,202 @@ my $keyword = "total_time";
 my $today   = `date -d "-1 day" +"%Y-%m-%d"`;
 $today      =~ tr/\n//d;
 
-
-my $dr_sites = {};
-my $date_g = "2013-03-24";
-my $fp;
-
 my $dbh;
-my ($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+my $do_db = 1;
 
-sub load_db_config
-{
-    my $conf_file_path = '/etc/cesu.conf';
-    open(DB_CONFIG, $conf_file_path) or die("ERR: can't open $conf_file_path : $!");
+my $date_g = "20130519";
 
-    while (<DB_CONFIG>) {
-        if (m/^DBHOST=(\S+)/) {
-            $dbhost = $1;	
-        }
-        elsif (m/^DBUSER=(\S+)/) {
-            $dbuser = $1;	
-        }
-        elsif (m/^DBPASS=(\S+)/) {
-            $dbpass = $1;
-        }
-        elsif (m/^DBNAME=(\S+)/) {
-            $dbname = $1;	
-        }
-        elsif (m/^DBPORT=(\S+)/) {
-            $dbport = $1;	
-        }
-    }
-}
+# record cluster data
+my $cluster_href;
 
-sub runSQL($)
-{
-    my @rec_a = ();
-    my @row;
-    my $sql = shift;
-    if (!defined($sql)) {
-        return;
-    }
+my %file_size = (
+    '0'  => '10',
+    '10' => '20',
+    '20' => '30',
+    '30' => '40',
+    '40' => '50',
+    '50' => '100',
+    '100' => '200',
+    '200' => '500',
+    '500' => '10000',
+);
 
-    #printf("RUNSQL: $sql\n");
-    my $sth = $dbh->prepare($sql);
-    $sth->execute() or die("SQL err: [$sql]" . "(" . length($sql) .")" . $sth->errstr);
-    while (@row = $sth->fetchrow_array) {
-        my @recs = @row;
-        push(@rec_a, \@recs);
-    }
-    $sth->finish();
 
-    return \@rec_a;
-}
 
- 
+$dbh = BMD::DBH->new(
+    'dbhost' => '116.213.78.228',
+    'dbuser' => 'cesutest',
+    'dbpass' => 'cesutest',
+    #'dbuser' => 'cesureadonly',
+    #'dbpass' => '66ecf9c968132321a02e6e7aff34ce5d',
+    'dbname' => 'speed',
+    'dbport' => 3306
+);
+
+
 use constant {
-    DOWN_SIZE   => 0,
-    DOWN_TIME   => 1,
-    DOWN_SPEED  => 2,
+    URL         => 0,
+    ROLE_ID     => 1,
+    MAIN_ID     => 2,
     ROLE_IP     => 3,
-    URL         => 4,
-    HEADER      => 5,
-    CLI_IP      => 6,
-    CLI_TYPE    => 7,
+    DOWNSPEED   => 4,
+    CITY_CODE   => 5,
+    CLIENT_IP   => 6,
+    DOWNSIZE    => 7,
+    TIME        => 8,
 };
-sub dr_one_site_elements($)
+
+
+sub list_hit_clusters()
 {
-    my $site_ref = shift;
-    my $cluster = "";
+    # find hit clusters
+    my $sql = qq/select fun_ipseg(role_ip),count(*) as c from speed_res_data_${date_g} where role_name like "%%_aqb" and header like "%%X-Powered-By-Anquanbao: HIT %%" and fun_ipseg(role_ip)!='0.0.0' group by fun_ipseg(role_ip) order by c desc/;
     
-    my $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header, client_ip, client_type from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
-        $site_ref->{aqb}{role_id}, $site_ref->{aqb}{monitor_time});
-    my $recs_aqb = runSQL($sql);
-    
-    $sql = sprintf("select down_size_bytes, down_time_s, down_speed_kbs, role_ip, url, header, client_ip, client_type from speed_monitor_res_data where mime_type like 'image/%%%%' and role_id=%d and monitor_time='%s';", 
-        $site_ref->{org}{role_id}, $site_ref->{org}{monitor_time});
-    my $recs_org = runSQL($sql);
+    my $recs = $dbh->query($sql);
+    for (my $i = 0; $i <= $#$recs; $i++) {
+        $cluster_href->{$recs->[$i][0]}{hit_cnt} = $recs->[$i][1];
+    }
 
-    for (my $i = 0; $i <= $#$recs_aqb; $i++) {
-        if ($recs_aqb->[$i]->[HEADER] =~ m/.*\^X-Powered-By-Anquanbao: HIT from (.*?)\^.*$/) {
-            $cluster = $1;
+    return 1;
+}
 
-            if ($recs_aqb->[$i]->[DOWN_TIME] == 0) {
-                $recs_aqb->[$i]->[DOWN_TIME] = -1;
-            }
+sub compare_hit_url($) 
+{
+    my $clusterip = shift;
+    my $downkbs_aqb;
+    my $downkbs_org;
+    my $roleip_aqb;
+    my $roleip_org;
+    my ($url, $main_id, $filesize);
 
-            LOOP: for (my $j = 0; $j <= $#$recs_org; $j++) {
-                if ($recs_org->[$j]->[URL] eq $recs_aqb->[$i]->[URL]) {
-                    if ($recs_org->[$j]->[DOWN_TIME] == 0) {
-                        $recs_org->[$j]->[DOWN_TIME] = -1;
+    # select hit url on cluster
+    my $sql = qq/select url,role_id,stat_main_id,role_ip,down_speed_kbs,city_code,client_ip,down_size_bytes,monitor_time from speed_res_data_${date_g} where role_name like "%%_aqb" and header like "%%X-Powered-By-Anquanbao: HIT %%" and fun_ipseg(role_ip)='$clusterip'/;
+
+    my $recs = $dbh->query($sql);
+    for (my $i = 0; $i <= $#$recs; $i++) {
+        $url = $recs->[$i][URL];
+        $main_id = $recs->[$i][MAIN_ID];
+        $downkbs_aqb = $recs->[$i][DOWNSPEED];
+        $roleip_aqb = $recs->[$i][ROLE_IP];
+        $filesize = $recs->[$i][DOWNSIZE];
+
+        ($downkbs_org, $roleip_org) = get_org_downspeed($url, $main_id);
+        if ($downkbs_org && $roleip_org) {
+            printf("$url $main_id $downkbs_aqb $roleip_aqb $downkbs_org $roleip_org\n");
+            
+            foreach my $k (sort keys %file_size) {
+                if (($filesize > ($k * 1000)) && ($filesize <= ($file_size{$k} * 1000))) {
+                    if ($downkbs_aqb >= $downkbs_org) {
+                        $cluster_href->{$clusterip}{"filesize_$k"}{fast} += 1;
+                        $cluster_href->{$clusterip}{fast_cnt} += 1;
+                    } else {
+                        $cluster_href->{$clusterip}{"filesize_$k"}{slow} += 1;
+                        $cluster_href->{$clusterip}{slowip}{$recs->[$i][CLIENT_IP]} += 1;
+                        $cluster_href->{$clusterip}{slow_cnt} += 1;
                     }
 
-                    if (($recs_aqb->[$i]->[DOWN_TIME]>1) && ($recs_aqb->[$i]->[DOWN_TIME]/$recs_org->[$j]->[DOWN_TIME]) > 1.5) {
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{downtime} = $recs_aqb->[$i]->[DOWN_TIME];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{downtime} = $recs_org->[$j]->[DOWN_TIME];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{ip} = $recs_aqb->[$i]->[ROLE_IP];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{ip} = $recs_org->[$j]->[ROLE_IP];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{cli_ip} = $recs_aqb->[$i]->[CLI_IP];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{cli_ip} = $recs_org->[$j]->[CLI_IP];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{aqb}{cli_type} = $recs_aqb->[$i]->[CLI_TYPE];
-                        $site_ref->{clusters}{$cluster}{$recs_aqb->[$i]->[URL]}{org}{cli_type} = $recs_org->[$j]->[CLI_TYPE];
-                    }
+                    $cluster_href->{$clusterip}{"filesize_$k"}{total} += 1;
+                    $cluster_href->{$clusterip}{"filesize_$k"}{aqb}{total_kbs} += $downkbs_aqb;
+                    $cluster_href->{$clusterip}{"filesize_$k"}{org}{total_kbs} += $downkbs_org;
 
-                    last LOOP;
+                    last;
                 }
             }
         }
     }
+
+    return 1;
 }
 
-use constant {
-    ROLE_ID      => 0,
-    TOTAL_TIME   => 1,
-    CITY_CODE    => 2,
-    MONITOR_TIME => 3,
-};
-sub dr_one_site($$)
+sub get_org_downspeed($$)
 {
-    my ($site, $hour) = @_;
-    # find the max total time cesu record
-    my $sql = sprintf("select role_id, total_time, city_code, monitor_time from speed_monitor_data where role_name='%s_aqb' and monitor_time>'%s %d:00:00' and monitor_time<'%s %d:00:00' and error_id=0 order by total_time desc limit 1;", 
-        $site, $date_g, $hour, $date_g, $hour+1);
+    my ($url, $main_id) = @_;
 
-    my $recs = runSQL($sql);
-    for (my $i = 0; $i <= $#$recs; $i++) {
-        $dr_sites->{$site}{aqb}{role_id} = $recs->[$i]->[ROLE_ID];
-        $dr_sites->{$site}{aqb}{total_time} = $recs->[$i]->[TOTAL_TIME];
-        $dr_sites->{$site}{aqb}{city_code} = $recs->[$i]->[CITY_CODE];
-        $dr_sites->{$site}{aqb}{monitor_time} = $recs->[$i]->[MONITOR_TIME];
-    }
+    # select same stat_main_id
+    my $sql = qq/select url,role_id,stat_main_id,role_ip,down_speed_kbs,city_code,client_ip,down_size_bytes,monitor_time from speed_res_data_${date_g} where role_name like '%%_ip' and url='$url' and stat_main_id=$main_id/;
 
-    $sql = sprintf("select role_id, total_time, city_code, monitor_time from speed_monitor_data where role_name='%s_ip' and monitor_time>'%s %d:00:00' and monitor_time<'%s %d:00:00' and error_id=0 and role_id=%d and city_code=%d;", 
-        $site, $date_g, $hour, $date_g, $hour+1, $dr_sites->{$site}{aqb}{role_id}+1, $dr_sites->{$site}{aqb}{city_code});
-    
-    $recs = runSQL($sql);
-    for (my $i = 0; $i <= $#$recs; $i++) {
-        $dr_sites->{$site}{org}{role_id} = $recs->[$i]->[ROLE_ID];
-        $dr_sites->{$site}{org}{total_time} = $recs->[$i]->[TOTAL_TIME];
-        $dr_sites->{$site}{org}{city_code} = $recs->[$i]->[CITY_CODE];
-        $dr_sites->{$site}{org}{monitor_time} = $recs->[$i]->[MONITOR_TIME];
-    }
-
+    my $recs = $dbh->query($sql);
     if ($#$recs != -1) {
-        if (($dr_sites->{$site}{aqb}{total_time}/$dr_sites->{$site}{org}{total_time}) > 1.5) {
-            dr_one_site_elements($dr_sites->{$site});
-        }
+        return ($recs->[0][DOWNSPEED], $recs->[0][ROLE_IP]);
     }
 
-    print Dumper($dr_sites->{$site});
-    
+    return;
 }
 
-sub log_one_site($)
+sub gen_speed_excel_data()
 {
-    my $site_k = shift;
-    if (exists($dr_sites->{$site_k}) && exists($dr_sites->{$site_k}->{clusters})) {
-        my $len = keys %{$dr_sites->{$site_k}->{clusters}};
-        if ($len > 0) { 
-            printf($fp "[$site_k]\n");
-            foreach my $srv_k (sort keys %{$dr_sites->{$site_k}->{clusters}}) {
-                printf($fp "$srv_k\n");
-                foreach my $e (sort keys %{$dr_sites->{$site_k}->{clusters}->{$srv_k}}) {
-                    printf($fp "\tURL: $e\n");
-                    printf($fp "\tORG: $dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{ip}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{downtime}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{cli_ip}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{org}{cli_type}\n");
-                    printf($fp "\tAQB: $dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{ip}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{downtime}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{cli_ip}, " .
-                        "$dr_sites->{$site_k}->{clusters}->{$srv_k}{$e}{aqb}{cli_type}\n");
-                }
+    my ($fast, $slow);
+
+    open(my $fp, ">/tmp/hit_downspeed.txt");
+   
+    printf($fp "cluster\t<10k\t10~20k\t20~30k\t30~40k\t40~50k\t50~100k\t100~200k\t200~500k\t>500k\tfast%%\tcount\n");
+
+    foreach my $k (sort{$cluster_href->{$b}{hit_cnt} <=> $cluster_href->{$a}{hit_cnt}} keys %$cluster_href) {
+        printf($fp "%s\t", $k);
+
+        foreach my $s (sort keys %file_size) {
+            $fast = 0;
+            $slow = 0;
+
+            if (exists($cluster_href->{$k}{"filesize_$s"})) {
+                $fast = $cluster_href->{$k}{"filesize_$s"}{fast} if exists($cluster_href->{$k}{"filesize_$s"}{fast});
+                $slow = $cluster_href->{$k}{"filesize_$s"}{slow} if exists($cluster_href->{$k}{"filesize_$s"}{slow});
+                printf($fp "%.2f\t", $fast * 100 / ($fast + $slow));
+            } else {
+                printf($fp "N/A\t");
             }
         }
-    }
-}
 
-
-open($fp, ">hit_download.result");
-$fp->autoflush(1);
-
-my $driver  = "DBI:mysql";
-load_db_config();
-# database connect
-$dbh = DBI->connect("$driver:database=$dbname;host=$dbhost;user=$dbuser;password=$dbpass;port=$dbport") or die("ConnDB err: " . DBI->errstr);
-
-open(my $fcesu, "<cesu.result");
-while (<$fcesu>) {
-    if (m/(.*?)\t(.*?)$/) {
-        if ($1 < 0) {
-            printf("### analysis $2 ...\n");
-            dr_one_site($2, 12);
-            log_one_site($2);
+        if (exists($cluster_href->{$k}{fast_cnt})) {
+            printf($fp "%.2f\t%d\n", 
+                $cluster_href->{$k}{fast_cnt} * 100 / ($cluster_href->{$k}{fast_cnt} + $cluster_href->{$k}{slow_cnt}), $cluster_href->{$k}{hit_cnt});
+        } else {
+            printf($fp "N/A\tN/A\n");
         }
     }
+
+    close($fp);
 }
-close($fcesu);
 
-$dbh->disconnect();
+sub gen_client_excel_data()
+{
+    my $slow_cnt;
+    open(my $fp, ">/tmp/hit_client.txt");
 
-close($fp);
+    foreach my $k (sort{$cluster_href->{$b}{hit_cnt} <=> $cluster_href->{$a}{hit_cnt}} keys %$cluster_href) {
+        $slow_cnt = 0;
+        printf($fp "[$k]\n");
+        
+        foreach my $p (sort keys %{$cluster_href->{$k}{slowip}}) {
+            $slow_cnt += $cluster_href->{$k}{slowip}{$p};
+        }
+        foreach my $p (sort keys %{$cluster_href->{$k}{slowip}}) {
+            printf($fp "\t%s %.2f%%\n", $p, $cluster_href->{$k}{slowip}{$p} * 100 / $slow_cnt);
+        }
+        printf("\n");
+    }
+
+    close($fp);
+}
+
+
+#
+# main start
+#
+list_hit_clusters();
+
+foreach my $k (sort{$cluster_href->{$a}{hit_cnt} <=> $cluster_href->{$b}{hit_cnt}} keys %$cluster_href) {
+    printf("$k => $cluster_href->{$k}{hit_cnt}\n");
+    compare_hit_url($k);
+    #last;
+}
+
+printf(Dumper($cluster_href));
+
+$dbh->fini();
+
+gen_speed_excel_data();
+gen_client_excel_data();
 
 1;
 
