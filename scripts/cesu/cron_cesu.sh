386fb607e80b262d94b08c3a36e9422a594d74b3
bekars <bekars@gmail.com>
2013-06-06 17:59:10 +0800

    update.

--- scripts/cesu/cron_cesu.sh
+++ scripts/cesu/cron_cesu.sh
@@ -2,7 +2,7 @@
 BASE_PATH=/home/apuadmin/baiyu
 
 ${BASE_PATH}/split_cesu_table.pl >/tmp/split_cesu_table.log 2>&1
-${BASE_PATH}/cesu_daily.pl --do_db >/tmp/cesu_daily.log 2>&1
+${BASE_PATH}/cesu_daily.pl --do_db --do_all >/tmp/cesu_daily.log 2>&1
 ${BASE_PATH}/analysis_daily.pl --do_db --do_analysis >/tmp/analysis_daily.log 2>&1
 ${BASE_PATH}/send_analysis_mail.pl >/tmp/send_analysis_mail.log 2>&1
 
