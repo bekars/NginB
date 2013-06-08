fbb7d9a356b6170a3e84860f2adf08c0d95c8e51
bekars <bekars@gmail.com>
2013-06-08 16:22:23 +0800

    update.

--- scripts/BMD/HTTP.pm
+++ scripts/BMD/HTTP.pm
@@ -43,12 +43,10 @@ sub query($)
     # a filehandle, reference to a scalar or reference to a typeglob can be used here.
     my $response_header;
     my $response_body;
-    my @body = ();
     $curl->setopt(CURLOPT_HEADERDATA, \$response_header);
     $curl->setopt(CURLOPT_WRITEDATA, \$response_body);
     #$curl->setopt(CURLOPT_TRANSFER_ENCODING, 1);
     #$curl->setopt(CURLOPT_WRITEFUNCTION, \&body_callback);
-    $curl->setopt(CURLOPT_FILE, \@body);
     #$curl->setopt(CURLOPT_ACCEPT_ENCODING, "gzip");
 
     # start the actual request
@@ -56,30 +54,35 @@ sub query($)
 
     # looking at the results...
     if ($retcode == 0) {
+        $response_body =~ m/^${response_header}(.*)$/;
+        $response_body = $1;
+        $response_header =~ s/%/%%/g;
+        $response_body =~ s/%/%%/g;
+
         # get http info
         $http_h{HEADER} = $response_header;
-        $http_h{BODY} = $response_body;
-        $http_h{HTTP_CODE} = $curl->getinfo(CURLINFO_HTTP_CODE);
-        $http_h{SIZE_DOWNLOAD} = $curl->getinfo(CURLINFO_SIZE_DOWNLOAD);
-        $http_h{SPEED_DOWNLOAD} = $curl->getinfo(CURLINFO_SPEED_DOWNLOAD);
-        $http_h{PRETRANSFER_TIME} = $curl->getinfo(CURLINFO_PRETRANSFER_TIME);
+        $http_h{BODY}   = $response_body;
+        $http_h{HTTP_CODE}          = $curl->getinfo(CURLINFO_HTTP_CODE);
+        $http_h{SIZE_DOWNLOAD}      = $curl->getinfo(CURLINFO_SIZE_DOWNLOAD);
+        $http_h{SPEED_DOWNLOAD}     = $curl->getinfo(CURLINFO_SPEED_DOWNLOAD);
+        $http_h{PRETRANSFER_TIME}   = $curl->getinfo(CURLINFO_PRETRANSFER_TIME);
         $http_h{STARTTRANSFER_TIME} = $curl->getinfo(CURLINFO_STARTTRANSFER_TIME);
-        $http_h{NAMELOOKUP_TIME} = $curl->getinfo(CURLINFO_NAMELOOKUP_TIME);
-        $http_h{TOTAL_TIME} = $curl->getinfo(CURLINFO_TOTAL_TIME);
-        $http_h{RESPONSE_CODE} = $curl->getinfo(CURLINFO_RESPONSE_CODE);
-        $http_h{REDIRECT_TIME} = $curl->getinfo(CURLINFO_REDIRECT_TIME);
-        $http_h{REDIRECT_URL} = $curl->getinfo(CURLINFO_REDIRECT_URL);
-        $http_h{LOCAL_IP} = $curl->getinfo(CURLINFO_LOCAL_IP);
-        $http_h{PRIMARY_IP} = $curl->getinfo(CURLINFO_PRIMARY_IP);
-        $http_h{CONNECT_TIME} = $curl->getinfo(CURLINFO_CONNECT_TIME);
-        $http_h{CONTENT_LENGTH} = $curl->getinfo(CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-        $http_h{CONTENT_TYPE} = $curl->getinfo(CURLINFO_CONTENT_TYPE);
-        $http_h{HEADER_IN} = $curl->getinfo(CURLINFO_HEADER_IN);
-        $http_h{HEADER_OUT} = $curl->getinfo(CURLINFO_HEADER_OUT);
-        $http_h{HEADER_SIZE} = $curl->getinfo(CURLINFO_HEADER_SIZE);
-        $http_h{FILETIME} = $curl->getinfo(CURLINFO_FILETIME);
-        $http_h{COOKIELIST} = $curl->getinfo(CURLINFO_COOKIELIST);
-        $http_h{HTTP_CONNECTCODE} = $curl->getinfo(CURLINFO_HTTP_CONNECTCODE);
+        $http_h{NAMELOOKUP_TIME}    = $curl->getinfo(CURLINFO_NAMELOOKUP_TIME);
+        $http_h{TOTAL_TIME}         = $curl->getinfo(CURLINFO_TOTAL_TIME);
+        $http_h{RESPONSE_CODE}      = $curl->getinfo(CURLINFO_RESPONSE_CODE);
+        $http_h{REDIRECT_TIME}      = $curl->getinfo(CURLINFO_REDIRECT_TIME);
+        $http_h{REDIRECT_URL}       = $curl->getinfo(CURLINFO_REDIRECT_URL);
+        $http_h{LOCAL_IP}           = $curl->getinfo(CURLINFO_LOCAL_IP);
+        $http_h{PRIMARY_IP}         = $curl->getinfo(CURLINFO_PRIMARY_IP);
+        $http_h{CONNECT_TIME}       = $curl->getinfo(CURLINFO_CONNECT_TIME);
+        $http_h{CONTENT_LENGTH}     = $curl->getinfo(CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+        $http_h{CONTENT_TYPE}       = $curl->getinfo(CURLINFO_CONTENT_TYPE);
+        $http_h{HEADER_IN}          = $curl->getinfo(CURLINFO_HEADER_IN);
+        $http_h{HEADER_OUT}         = $curl->getinfo(CURLINFO_HEADER_OUT);
+        $http_h{HEADER_SIZE}        = $curl->getinfo(CURLINFO_HEADER_SIZE);
+        $http_h{FILETIME}           = $curl->getinfo(CURLINFO_FILETIME);
+        $http_h{COOKIELIST}         = $curl->getinfo(CURLINFO_COOKIELIST);
+        $http_h{HTTP_CONNECTCODE}   = $curl->getinfo(CURLINFO_HTTP_CONNECTCODE);
 
         #print("Transfer went ok\n");
         # judge result and next action based on $response_code
--- scripts/BMD/IPOS.pm
+++ scripts/BMD/IPOS.pm
@@ -3,10 +3,14 @@
 package BMD::IPOS;
 
 use strict;
-use BMD::DBH;
+use utf8;
 use autodie;
 use Try::Tiny;
 use Data::Dumper;
+use Encode;
+use BMD::DBH;
+use BMD::HTTP;
+use JSON -support_by_pp;
 
 my $dbh;
 my $debug = 1;
@@ -125,98 +129,6 @@ sub load($)
     return 1;
 }
 
-sub load2($)
-{
-    my $self = shift;
-    my $ipdb = shift;
-
-    my @rec_a;
-    my $recs;
-    my $sql;
-
-    if ($ipdb eq "DB") {
-        $dbh = BMD::DBH->new(
-            'dbhost' => '116.213.78.197',
-            'dbuser' => 'readonly',
-            'dbpass' => 'anQuanba0sp11d',
-            'dbname' => 'ip',
-            'dbport' => 3306
-        );
-
-        $self->{dbh} = $dbh;
-        $self->{dbh}->execute("set names utf8");
-    }
-
-    printf("### load ip pos\n") if $debug;
-    if ($ipdb eq "DB") {
-        $sql = qq/select id,ipstart,ipend,countryid,provinceid,ispid from ip/;
-        $self->{ip_pos} = $self->{dbh}->query($sql);
-    } else {
-        open(my $ipfp, "<$ipdb/ip.db") or die("ERR: can not open $ipdb/ip.db!\n");
-        while (<$ipfp>) {
-            $_ =~ m/^(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\n$/;
-            $self->{ip_pos}{$2}{country} = $4;
-            $self->{ip_pos}{$2}{province} = $5;
-            $self->{ip_pos}{$2}{isp} = $6;
-        }
-        close($ipfp);
-    }
-
-    printf("### load country\n") if $debug;
-    if ($ipdb eq "DB") {
-        $sql = qq/select id,country from country/;
-        $recs = $self->{dbh}->query($sql);
-        for (my $i = 0; $i <= $#$recs; $i++) {
-            $self->{country}->{$recs->[$i][ID]} = $recs->[$i][VAL];
-        }
-    } else {
-        open(my $cfp, "<$ipdb/country.db") or die("ERR: can not open $ipdb/country.db!\n");
-        while (<$cfp>) {
-            $_ =~ m/^(.*?)\t(.*?)\n$/;
-            $self->{country}->{$1} = $2;
-        }
-        close($cfp);
-    }
-
-    printf("### load province\n") if $debug;
-    if ($ipdb eq "DB") {
-        $sql = qq/select id,province from province/;
-        $recs = $self->{dbh}->query($sql);
-        for (my $i = 0; $i <= $#$recs; $i++) {
-            $self->{province}->{$recs->[$i][ID]} = $recs->[$i][VAL];
-        }
-    } else {
-        open(my $pfp, "<$ipdb/province.db") or die("ERR: can not open $ipdb/province.db!\n");
-        while (<$pfp>) {
-            $_ =~ m/^(.*?)\t(.*?)\n$/;
-            $self->{province}->{$1} = $2;
-        }
-        close($pfp);
-    }
-
-    printf("### load isp\n") if $debug;
-    if ($ipdb eq "DB") {
-        $sql = qq/select id,isp from isp/;
-        $recs = $self->{dbh}->query($sql);
-        for (my $i = 0; $i <= $#$recs; $i++) {
-            $self->{isp}->{$recs->[$i][ID]} = $recs->[$i][VAL];
-        }
-    } else {
-        open(my $ifp, "<$ipdb/isp.db") or die("ERR: can not open $ipdb/isp.db!\n");
-        while (<$ifp>) {
-            $_ =~ m/^(.*?)\t(.*?)\n$/;
-            $self->{isp}->{$1} = $2;
-        }
-        close($ifp);
-    }
-
-    if ($ipdb eq "DB") {
-        $self->{dbh}->fini();
-    }
-
-    return 1;
-}
-
 my @ip_cache = ();
 
 # input ip address like 
@@ -276,6 +188,25 @@ sub query($)
     return ("UFO", "UFO", "UFO");
 }
 
+sub load2($)
+{
+    my $self = shift;
+    my $ipdb = shift;
+
+    printf("### load ip pos\n") if $debug;
+    open(my $ipfp, "<$ipdb/ipseg.db") or die("ERR: can not open $ipdb/ipseg.db!\n");
+    while (<$ipfp>) {
+        $_ =~ m/^(.*?):(.*?):(.*?):(.*?):(.*?):(.*?)\n$/;
+        $self->{ip_pos}{$1}{country} = $2;
+        $self->{ip_pos}{$1}{region} = $3;
+        $self->{ip_pos}{$1}{province} = $4;
+        $self->{ip_pos}{$1}{city} = $5;
+        $self->{ip_pos}{$1}{isp} = $6;
+    }
+    close($ipfp);
+    return 1;
+}
+
 sub query2($)
 {
     my $self = shift;
@@ -284,30 +215,19 @@ sub query2($)
 
     $ip .= '.0' if (2 == ($ip =~ tr/././));
     my $ip_pos = $self->{ip_pos};
-    my $country = $self->{country};
-    my $province = $self->{province};
-    my $isp = $self->{isp};
 
     $ip =~ m/^(\d+?)\.(\d+?)\.(\d+?)\.(\d+?).*/;
+    $ip = "$1.$2.$3.0";
     my $ipnum = $1*256*256*256 + $2*256*256 + $3*256 + $4;
 
-printf("### $ipnum\n");
-    if (($ipnum >= 1778515968) && ($ipnum <= 1778647039)) {
-        $c_data = $country->{2};
-        $p_data = $province->{4};
-        $i_data = "UFO";
+    if (exists($ip_pos->{$ip})) {
+        printf("### $ip $ipnum $ip_pos->{$ip}->{country} $ip_pos->{$ip}->{province} $ip_pos->{$ip}->{isp}\n") if $debug;
+        $c_data = $ip_pos->{$ip}->{country};
+        $p_data = $ip_pos->{$ip}->{province};
+        $i_data = $ip_pos->{$ip}->{isp};
         return ($c_data, $p_data, $i_data);
-    }
-
-    foreach my $k (sort {$a<=>$b} keys %$ip_pos) {
-        if ($ipnum > $k) {
-            #printf("### $ip $ipnum $ip_pos->[$i]->[COUNTRY] $ip_pos->[$i]->[PROVINCE] $ip_pos->[$i]->[ISP]\n") if $debug;
-            $c_data = $country->{$ip_pos->{$k}->{country}};
-            $p_data = $province->{$ip_pos->{$k}->{province}};
-            $i_data = $isp->{$ip_pos->{$k}->{isp}};
-            $i_data = "UFO" if $i_data eq "";
-            return ($c_data, $p_data, $i_data);
-        }
+    } else {
+        printf("### $ip $ipnum NOT EXISTS ###\n") if $debug;
     }
 
     return ($c_data, $p_data, $i_data);
@@ -340,6 +260,40 @@ sub query_id($)
     }
 
     return (0, 0, 0);
+}
+
+sub query_taobao($)
+{
+    my $self = shift;
+    my $ip = shift;
+
+    $ip .= '.0' if (2 == ($ip =~ tr/././));
+    $ip =~ m/^(\d+?)\.(\d+?)\.(\d+?)\.(\d+?).*/;
+    my $ipnum = $1*256*256*256 + $2*256*256 + $3*256 + $4;
+
+    my $http = undef;
+    my $query_url = sprintf("http://ip.taobao.com/service/getIpInfo.php?ip=%s", $ip);
+    my $http_hld = BMD::HTTP->new();
+
+    LOOP: for (my $i = 0; $i < 10; ++$i) {
+        $http = $http_hld->query($query_url);
+        last LOOP if ($http);
+        sleep(1);
+    }
+
+    my $json = from_json($http->{BODY}, {utf8=>1});
+    if ($json->{code} != 0) {
+        return undef;
+    }
+
+    $json->{data}{country} = encode("utf8", $json->{data}{country});
+    $json->{data}{area}    = encode("utf8", $json->{data}{area});
+    $json->{data}{region}  = encode("utf8", $json->{data}{region});
+    $json->{data}{county}  = encode("utf8", $json->{data}{county});
+    $json->{data}{city}    = encode("utf8", $json->{data}{city});
+    $json->{data}{isp}     = encode("utf8", $json->{data}{isp});
+    $json->{data}{ipnum}   = $ipnum;
+    return $json->{data};
 }
 
 sub destroy()
