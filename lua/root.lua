1a1c1a29adb8ea1fc5f8d542c97fd3d2c6e48da0
bekars <bekars@gmail.com>
2014-08-07 17:18:06 +0800

    update.

---
+++ lua/root.lua
@@ -0,0 +1,10 @@
+--[[
+
+lua.baiyu.org.cn root
+
+--]]
+
+ngx.header["Content-Type"] = "text/html";
+--ngx.header["Content-Length"] = "45";
+ngx.say("<html><body><b>Lua Test !!!</b></body></html>");
+
